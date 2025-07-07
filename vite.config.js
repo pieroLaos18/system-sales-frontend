dbeb218f5a2b5d4a38c07d64ddb394f7b7f8877e
Piero Alexandro Laos Saravia <105088763+pieroLaos18@users.noreply.github.com>
2025-07-07 03:59:43 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
  
 export default defineConfig({
-  base: './', // ✅ Importante para Azure
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
